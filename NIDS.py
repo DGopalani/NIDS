01df3516960516be946527f89df3c9e572874513
DGopalani <114025985+DGopalani@users.noreply.github.com>
2023-11-04 18:36:01 -0700

    Working for UDPLag

--- NIDS.py
+++ NIDS.py
@@ -10,20 +10,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler 
 
 #Define the input CSV file path
-csv_file_path = 'Data/03-11/UDPLag.csv' 
+UDPLag_csv_file_path = 'Data/01-12/UDPLag.csv'
 
-df = pd.read_csv(csv_file_path)
+UDPLag_df = pd.read_csv(UDPLag_csv_file_path)
 
 # Select only numeric columns from the DataFrame
-numeric_df = df.select_dtypes(include=['number'])
+numeric_df = UDPLag_df.select_dtypes(include=['number'])
 
-'''
+
 #df of categorical rows
 # Filter the columns in df_subset that are not in numeric_df
-categorical_columns = df.columns.difference(numeric_df.columns)
-categorical_df = df[categorical_columns]
-print(categorical_df.columns)
+categorical_columns = UDPLag_df.columns.difference(numeric_df.columns)
+categorical_df = UDPLag_df[categorical_columns]
 
+#UDP training dataset
+UDP_csv_file_path = 'Data/01-12/DrDoS_UDP.csv'
+UDP_df = pd.read_csv(UDP_csv_file_path)
+
+'''
+#make bar graphs of categorical data
 for column in categorical_df.columns:
     value_counts = categorical_df[column].value_counts()
     plt.figure(figsize=(8, 6))  # Adjust the figure size if needed
@@ -33,19 +38,15 @@ for column in categorical_df.columns:
     plt.ylabel('Unique Values')
     plt.title(f'Count of Unique Values in {column}')
     plt.show()
-'''
 
-#numeric df stuff
+#make correlation matrix
 correlation_matrix = numeric_df.corr()
 
-
 # Create a heatmap to visualize the correlation matrix
-'''
 plt.figure(figsize=(8, 8))  # Adjust the figure size if needed
 sns.heatmap(correlation_matrix, annot=False, cmap="coolwarm", linewidths=0.5)
 plt.title("Correlation Heatmap (Numeric Data Only)")
 plt.show()
-'''
 
 # Set a threshold for high correlation (e.g., 0.7)
 threshold = 0.7
@@ -59,12 +60,10 @@ for i in range(len(correlation_matrix.columns)):
 
 
 # Print the pairs of variables with high correlation
-'''
 for pair in high_correlations:
     var1, var2 = pair
     correlation_value = correlation_matrix.loc[var1, var2]
     print(f"High correlation between {var1} and {var2}: {correlation_value}")
-'''
 
 #keep only one of each pair of vars with high correlation
 vars_to_keep = []
@@ -72,37 +71,44 @@ for pair in high_correlations:
     var1, var2 = pair
     if var1 not in vars_to_keep and var2 not in vars_to_keep:
         vars_to_keep.append(var1)
-
 #add Label to vars_to_keep
 vars_to_keep.append(' Label')
+'''
 
 #apply feature mappin on label column
-df[' Label'] = df[' Label'].map({'BENIGN': 0, 'UDPLag': 1, 'UDP': 2, 'Syn': 3})
+UDPLag_df[' Label'] = UDPLag_df[' Label'].map({'BENIGN': 0, 'UDP-lag': 1, 'UDP': 2, 'Syn': 3, 'WebDDoS': 4})
+UDP_df[' Label'] = UDP_df[' Label'].map({'BENIGN': 0, 'UDP-lag': 1, 'UDP': 2, 'Syn': 3, 'WebDDoS': 4})
 
-#new filter list based on dataset info
-vars_to_keep = [' ACK Flag Count', '_bInit_Winytes_forward', ' min_seg_size_forward', ' Fwd IAT Mean', ' Fwd IAT Max', ' Label']
+#make column filter list based on dataset info
+UDPLag_vars_to_keep = [' ACK Flag Count', 'Init_Win_bytes_forward', ' min_seg_size_forward', ' Fwd IAT Mean', ' Fwd IAT Max', ' Label']
+UDP_vars_to_keep = [' ACK Flag Count', 'Init_Win_bytes_forward', ' min_seg_size_forward', ' Fwd IAT Mean', ' Fwd IAT Max', ' Label']
 
-#filter df based on vars_to_keep
-df_filtered = df[vars_to_keep]
+#filter df based on filter list defined above
+UDPLag_df_filtered = UDPLag_df[UDPLag_vars_to_keep]
 
-#Prepare train data
-y_train = df_filtered[[' Label']]
-X_train = df_filtered.drop(columns=[' Label'], axis=1)
-sc = MinMaxScaler() 
-X_train = sc.fit_transform(X_train)
+UPD_df_filtered = UDP_df[UDP_vars_to_keep]
 
 
 #test dataset
-test_csv_file_path = 'Data/01-12/UDPLag.csv'
+test_csv_file_path = 'Data/03-11/UDPLag.csv'
 test_df = pd.read_csv(test_csv_file_path)
 
 #apply feature mappin on label column
-print(test_df[' Label'].unique())
-test_df[' Label'] = test_df[' Label'].map({'BENIGN': 0, 'UDP-lag': 1, 'WebDDoS': 2})
+test_df[' Label'] = test_df[' Label'].map({'BENIGN': 0, 'UDPLag': 1, 'UDP': 2, 'Syn': 3, 'WebDDoS': 4})
+#rename wrongly named column for consistency
+test_df.rename(columns={'_bInit_Winytes_forward': 'Init_Win_bytes_forward'}, inplace=True)
 
-#test filtered df based on vars_to_keep
-test_vars_to_keep = [' ACK Flag Count', 'Init_Win_bytes_forward', ' min_seg_size_forward', ' Fwd IAT Mean', ' Fwd IAT Max', ' Label']
-test_df_filtered = test_df[test_vars_to_keep]
+#make testing filtered df based on vars_to_keep
+test_df_filtered = test_df[UDPLag_vars_to_keep]
+
+#keep only 0, 1, and 4 in Label column of test_df_filtered
+test_df_filtered = test_df_filtered[test_df_filtered[' Label'].isin([0, 1, 4])]
+
+#Prepare train data
+y_train = UDPLag_df_filtered[[' Label']]
+X_train = UDPLag_df_filtered.drop(columns=[' Label'], axis=1)
+sc = MinMaxScaler() 
+X_train = sc.fit_transform(X_train)
 
 #Prepare test data
 y_test = test_df_filtered[[' Label']]
@@ -110,14 +116,20 @@ X_test = test_df_filtered.drop(columns=[' Label'], axis=1)
 sc = MinMaxScaler()
 X_test = sc.fit_transform(X_test)
 
+
+#check for null values
+print(np.isnan(y_test.values.any()))
+print(np.isfinite(y_test.values.all()))
+ 
 #print data shapes
 print(X_train.shape, X_test.shape) 
 print(y_train.shape, y_test.shape) 
 
 #Train on Random Forest Model
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score 
+from sklearn.metrics import accuracy_score
 
+print("Random Forest Classifier")
 clfr = RandomForestClassifier(n_estimators = 5) 
 start_time = time.time() 
 clfr.fit(X_train, y_train.values.ravel()) 
@@ -133,4 +145,38 @@ print("Testing time: ", end_time-start_time)
 #Print results
 print("Train score is:", clfr.score(X_train, y_train)) 
 print("Test score is:", clfr.score(X_test, y_test)) 
+'''
+print("Gaussian Naive Bayes Classifier")
+# Gaussian Naive Bayes 
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.metrics import accuracy_score 
+  
+clfg = GaussianNB() 
+start_time = time.time() 
+clfg.fit(X_train, y_train.values.ravel()) 
+end_time = time.time() 
+print("Training time: ", end_time-start_time) 
 
+start_time = time.time() 
+y_test_pred = clfg.predict(X_train) 
+end_time = time.time() 
+print("Testing time: ", end_time-start_time) 
+
+print("Train score is:", clfg.score(X_train, y_train)) 
+print("Test score is:", clfg.score(X_test, y_test)) 
+'''
+
+# Decision Tree 
+print("Decision Tree Classifier") 
+from sklearn.tree import DecisionTreeClassifier 
+clfd = DecisionTreeClassifier(criterion ="entropy", max_depth = 4) 
+start_time = time.time() 
+clfd.fit(X_train, y_train.values.ravel()) 
+end_time = time.time() 
+print("Training time: ", end_time-start_time) 
+start_time = time.time() 
+y_test_pred = clfd.predict(X_train) 
+end_time = time.time() 
+print("Testing time: ", end_time-start_time) 
+print("Train score is:", clfd.score(X_train, y_train)) 
+print("Test score is:", clfd.score(X_test, y_test)) 
